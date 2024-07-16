e6c0896ab63b7e9e46c8a3244bc0d7d2cc97c9e9
ceo-uploads <ceo.at.unrealstudio@gmail.com>
2024-07-17 00:34:58 +0600

    Create Main.java
    Main Class

---
+++ Printing Patterns/Printing Lower Diagonal Triangle/JavaFile/Main.java
@@ -0,0 +1,15 @@
+package JavaFile;
+import java.util.Scanner;
+
+class Main {
+  publc static void main(String[] arg) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter The Number of Length: ");
+    int number = input.nextInt();
+
+    Triangle tn = new Triangle(number);
+
+    tn.view();
+  }
+}
